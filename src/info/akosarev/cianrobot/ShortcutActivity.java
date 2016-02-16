2ca9e043b67b848575b5c5bab75a490c687f3c67
Alexey Kosarev <alex@homyaki.info>
2016-02-16 15:19:57 +0300

    First stage of scanner

---
+++ src/info/akosarev/cianrobot/ShortcutActivity.java
@@ -0,0 +1,30 @@
+package info.akosarev.cianrobot;
+
+
+import info.akosarev.cianrobot.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.Intent.ShortcutIconResource;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ShortcutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // The meat of our shortcut
+        Intent shortcutIntent = new Intent(this, CianActivity.class); 
+        ShortcutIconResource iconResource = Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher);
+         
+        // The result we are passing back from this activity
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "Shortcut Test");
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
+        setResult(RESULT_OK, intent);
+
+        this.finish();
+    }
+
+}
